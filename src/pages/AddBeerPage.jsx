84e71c86ca6fa1cdacb6404c67a248882e2f6976
Anna Turek <anutkakpi@ukr.net>
2024-11-16 17:57:45 +0200

    in Process

--- src/pages/AddBeerPage.jsx
+++ src/pages/AddBeerPage.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import axios from "axios";
 
 function AddBeerPage() {
   // State variables to store the values of the form inputs. You can leave these as they are.
@@ -21,16 +22,49 @@ function AddBeerPage() {
   const handleAttenuationLevel = (e) => setAttenuationLevel(e.target.value);
   const handleContributedBy = (e) => setContributedBy(e.target.value);
 
-
-
   // TASK:
   // 1. Create a function to handle the form submission and send the form data to the Beers API to create a new beer.
   // 2. Use axios to make a POST request to the Beers API.
   // 3. Once the beer is created, navigate the user to the page showing the list of all beers.
 
-
-
   // Structure and the content of the page showing the form for adding a new beer. You can leave this as it is.
+  //Axios post request
+  function handleAddPost(e) {
+    //first always stop the form from refreshing
+    e.preventDefault();
+    //second construct your object that you want to send
+    const postToCreate = {
+      name,
+      tagline,
+      description,
+      image_url: imageUrl,
+      first_brewed: firstBrewed,
+      brewers_tips: brewersTips,
+      attenuation_level: attenuationLevel,
+      contributed_by: contributedBy,
+    };
+    console.log("Adding a post", postToCreate);
+    //sending a POST request with fetch
+    fetch("https://ih-beers-api2.herokuapp.com/beers/add", {
+      method: "POST", //the other types would be (GET, PUT, PATCH, DELETE)
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(postToCreate),
+    })
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        console.log(data);
+      })
+      .catch((err) => console.log(err));
+    axios
+      .post("https://ih-beers-api2.herokuapp.com/beers/add", postToCreate)
+      .then(({ data }) => {
+        console.log(data);
+      })
+      .catch((err) => console.log(err));
+  }
+
   return (
     <>
       <div className="d-inline-flex flex-column w-100 p-4">
